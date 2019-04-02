a0b4f819d41feb0c85e3bf9253f045a24d91cdae
rebenely <venrayyle@gmail.com>
2019-04-02 15:57:43 +0800

    debug shit

--- authJWT.js
+++ authJWT.js
@@ -212,6 +212,7 @@ module.exports  = {
               var end = moment().utc().utcOffset(8);
               var startDate = moment(start, 'MM/DD/YYYY, LTS');
               var playTime = Math.round((end - startDate)/1000);
+              console.log(moment().utc().utcOffset(8).add(24, 'hours').add(1, 'minutes'));
               console.log(startDate.format('MM/DD/YYYY, LTS'), 'end', end.format('MM/DD/YYYY, LTS'), 'diff', typeof playTime, ':', playTime);
               if(payload.idle == undefined || payload.idle == null) {
                   payload.idle = 0;
